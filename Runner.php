886c0bdc56be229c6294762aed862f2c80897d10
danyRafina <danyrafina@gmail.com>
2017-12-24 18:45:21 +0900

    Add callable Fis without CLI

---
+++ Runner.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ *
+ *  * This is an iumio Framework component
+ *  *
+ *  * (c) RAFINA DANY <dany.rafina@iumio.com>
+ *  *
+ *  * iumio Framework, an iumio component [https://iumio.com]
+ *  *
+ *  * To get more information about licence, please check the licence file
+ *
+ */
+
+/**
+ *
+ *  * This is an iumio Framework component
+ *  *
+ *  * (c) RAFINA DANY <danyrafina@gmail.com>
+ *  *
+ *  * iumio Framework - iumio Components
+ *  *
+ *  * To get more information about licence, please check the licence file
+ *
+ */
+
+namespace iumioFramework\Fis;
+use iumioFramework\Core\Console\Display\OutputManager as Output;
+
+/**
+ * Class Runner
+ * This class able to run the Framework internal server (Fis)
+ * @package iumioFramework\Fis
+ * @category Framework
+ * @licence  MIT License
+ * @link https://framework.iumio.com
+ * @author   RAFINA Dany <dany.rafina@iumio.com>
+ */
+
+class Runner
+{
+    /** Run the Framework internal server
+     * @param string|null $host The host name or IP
+     * @param int|null $port The port
+     * @param bool $https If use https or not (required if https, the cert)
+     * @param string|null $root The root directory
+     * @param string|null $router The router
+     * @param null $cert The SSL certificate if needed
+     * @param int $cluster The server cluster : by default set to 10
+     */
+    public function run(string $host = null, int $port = null, bool $https = false,
+                        string $root = null, string $router = null, $cert = null, int $cluster = 10) {
+
+        if (is_null($host) && is_null($port)) {
+            $servers = ["localhost:8000"];
+        }
+        elseif (is_null($host) && !is_null($port)) {
+            $servers = ["localhost:$port"];
+        }
+        elseif (!is_null($host) && is_null($port)) {
+            $servers = ["$host:8000"];
+        }
+        else {
+            $servers = ["$host:$port"];
+        }
+
+        if ($https) {
+            $secures = $servers;
+            $servers = [];
+        }
+        else {
+            $secures = [];
+        }
+        $docroot = is_null($root)? realpath(__DIR__.'/../../public') : $root;
+        $number  = $cluster;
+        $cert    = is_null($cert) ? (__DIR__ . '/certificate.pem') : $cert;
+
+        if (!$servers && !$secures) {
+            Output::displayAsError("Framework internal server Error : At least 1 server must be specified.\n
+            Referer to help command to get options list\n");
+        }
+
+        try {
+
+            if ($number < 1 || $number > 20) {
+                throw new \RuntimeException('The number of clusters must be between 1 and 20.');
+            }
+            if ($docroot !== null && !is_dir($docroot)) {
+                throw new \RuntimeException("No such document root directory: ddd");
+            }
+            if ($router !== null && !is_file($router)) {
+                throw new \RuntimeException("No such router script file: $router");
+            }
+            if (!is_file($cert)) {
+                throw new \RuntimeException("No such certificate file: $cert");
+            }
+            if (!openssl_pkey_get_public("file://$cert")) {
+                throw new \RuntimeException("Invalid certificate file: $cert");
+            }
+
+            $listeners = [];
+            foreach ([$servers, $secures] as $type => $group) {
+                foreach ($group as $i => $server) {
+                    list($host, $port) = explode(':', $server, 2) + [1 => ''];
+                    $ip = filter_var(gethostbyname($host), FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+                    $regex = '/\A(?:[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])\z/';
+                    if ($ip === false || !preg_match($regex, $port)) {
+                        throw new \RuntimeException("Invalid host or port: $server");
+                    }
+                    if (isset($listeners[$server])) {
+                        throw new \RuntimeException("Duplicated entry: $server");
+                    }
+                    $listeners[$server] = [$host, $port, $type === 1, $cert];
+                }
+            }
+
+            $loop = \React\EventLoop\Factory::create();
+
+            $used_processes = [];
+            $factory = new \mpyw\HyperBuiltinServer\BuiltinServerFactory($loop);
+            $factory
+                ->createMultipleAsync($number, '127.0.0.1', $docroot, $router)
+                ->then(function (array $processes) use ($loop, $listeners, &$used_processes) {
+                    $used_processes = $processes;
+                    $master = new \mpyw\HyperBuiltinServer\Master($loop, $processes);
+                    foreach ($listeners as $listener) {
+                        call_user_func_array([$master, 'addListener'], $listener);
+                    }
+                })
+                ->then(null, function ($e) use (&$used_processes) {
+                    foreach ($used_processes as $process) {
+                        $process->terminate();
+                    }
+                    throw $e;
+                })
+                ->done();
+
+            set_time_limit(0);
+            $loop->run();
+
+        } catch (\Throwable $e) {
+            Output::displayAsError("Framework internal server Error :  {$e->getMessage()}");
+
+        } catch (\Exception $e) {
+
+            Output::displayAsError("Framework internal server Error :  {$e->getMessage()}");
+            exit(1);
+
+        }
+    }
+}
